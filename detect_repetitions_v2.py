6cf2d0b4bc99f60176f969b971ff576215ff3407
dprice80 <dprice80@gmail.com>
2018-09-11 11:57:22 +0100

    Before using entropy as selection criteria

--- detect_repetitions_v2.py
+++ detect_repetitions_v2.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 
+# TO DO
+# issue of overlapping components not addressed.
+# need a better way to select correct number of components. Choosing 0.1 as
+# percentage change limit is probably not good enough. Could use baysian method
+# perhaps AIC?
+
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import permutations
 import scipy.io as io
 from scipy import signal
+import entropydecomp as ed
 
 # io.loadmat("")
 
 N = 10000
 step = 1
 Lvec = 5
-Lsig = 30
-randscaling = 0.3
+Lsig = 15
+randscaling = 0.1
 
 rn = np.random.randn(N) * randscaling
+rn_noise = np.copy(rn)
 rno = np.zeros(N)
 
 z = np.sin(np.linspace(-np.pi + 0.4, np.pi + 0.4, Lsig)) # shift signal by 45 deg
@@ -22,26 +30,11 @@ z = np.sin(np.linspace(-np.pi + 0.4, np.pi + 0.4, Lsig)) # shift signal by 45 de
 for i in range(0, N, 50):
     rno[i:i + Lsig] = z
 
-f1 = 1/np.linspace(0, 100, len(rn)+1)[1:] \
-     * np.exp(-1j * 2 * np.pi * np.random.rand(len(rn)))
-f1[int(len(rn)/2)+1:] = 0
-rno = np.fft.ifft(f1).real
-
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = signal.butter(order, [low, high], btype='band')
-    return b, a
-
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = signal.lfilter(b, a, data)
-    return y
-
-rn = butter_bandpass_filter(rn, 10, 100, 1000, 5).real
-
+# f1 = 1/np.linspace(0, 100, len(rn)+1)[1:] \
+#      * np.exp(-1j * 2 * np.pi * np.random.rand(len(rn)))
+# f1[int(len(rn)/2)+1:] = 0
+# rno = np.fft.ifft(f1).real
+# rn = ed.butter_bandpass_filter(rn, 10, 100, 1000, 5).real
 
 # plt.plot(f1)
 # plt.show()
@@ -56,139 +49,48 @@ for t in l:
     pkeys.append("%s%s%s%s%s" % t)
 
 
-# define ranking function
-def rankdata(data, pkeys, rstep=1):
-    Ns = len(data)
-    perms = {pk: [] for pk in pkeys}
-    for i in range(0, Ns - Lvec * rstep):
-        ranks = data[i:i + Lvec * rstep:rstep].transpose().argsort()
-        a = ''.join(map(str, ranks))
-        perms[a].append(i)
-
-    permn = []
-    for key in pkeys:
-        permn.append(len(perms[key]))
-    permn = np.array(permn)
-
-    return (perms, permn)
-
-
-def find(a):
-    return [i for (i, val) in enumerate(a) if val == True]
-
-
-# find average of component
-# for each permutation of rank
-def generatesigmean(permind, rstep):
-    # check if instance has more than N repetitions
-    sigmean = np.zeros(Lvec)
-    ls = len(perms[pkeys[permind]])
-    L = len(rn)
-    for ii in perms[pkeys[permind]]:
-        ind = range(ii, ii+(Lvec*rstep),rstep)
-        if ind[-1] <= len(rn):
-            sigmean = sigmean + rn[ind] / ls
-    return sigmean
-
-
-def concatnplist(nplist):
-    nout = np.zeros([len(nplist), N])
-    for ii in range(0,len(nplist)):
-        nout[ii, :] = nplist[ii].flatten()
-    return nout
-
-
-def sampentropy(U, m, r):
-    def _maxdist(x_i, x_j):
-        result = max([abs(ua - va) for ua, va in zip(x_i, x_j)])
-        return result
-
-    def _phi(m):
-        x = [[U[j] for j in range(i, i + m - 1 + 1)] for i in range(N - m + 1)]
-        C = [len([1 for j in range(len(x)) if i != j and _maxdist(x[i], x[j]) <= r]) for i in range(len(x))]
-        return sum(C)
-
-    N = len(U)
-
-    return -np.log(_phi(m + 1) / _phi(m))
-
-
 plt.interactive(False)
 plt.close("all")
 
 rnn = np.copy(rn)
 
-newsigc = np.zeros(rn.shape)
-rstep = 2**np.array(range(0,7,1))
+
+rstep = 2**np.array(range(0, 7, 1))
+# rstep = np.array(range(1, 8, 1))
+
 sampen = [1e6]
 sampendiff = 1
 ri = 0
 
-while sampendiff > 0.05:
-    rsi = np.mod(ri,5)
-    newsigall = []
-    newsigcall = []
-    for rsi in range(len(rstep)):
-        rncand = np.copy(rn)  # candidate rn
-        newsigccand = np.copy(newsigc)
-        print("Calculating spacing %d" % rstep[rsi])
-        perms, permn = rankdata(rn, pkeys, rstep=rstep[rsi])
-        psi = permn.argsort()[::-1]
-        permncumsum = permn[psi].cumsum()
-        sigtemp = list()
-        permsfit = {k: [] for k in pkeys}
-        newsig = np.zeros(rn.shape)
-        pi = psi[0] # choose the vector with most occurences
-        sig = generatesigmean(pi, rstep[rsi])
-        # For each instance of vector (ppi is the index in rn)
+newsig = np.zeros(rn.shape)
 
-        # calculate polyfit for each location
-        for ppi in perms[pkeys[pi]]:
-            rind = range(ppi, ppi + Lvec * rstep[rsi], rstep[rsi])
-            permsfit[pkeys[pi]].append(
-                np.polyfit(sig.flatten(), rn[rind], 1))
-
-        # calculate regression at each location of chosen motif
-        for pfi in range(0, len(perms[pkeys[pi]])):
-            ppi = perms[pkeys[pi]][pfi]
-            rind = range(ppi, ppi + Lvec * rstep[rsi], rstep[rsi])
-            ysig = sig * permsfit[pkeys[pi]][pfi][0] + permsfit[pkeys[pi]][pfi][1]
-            newsig[rind] += ysig.flatten()
-
-        # Delete extracted components from original source timeseries
-        rncand -= newsig # candidate rn
-        newsigccand += newsig # clean signal
-        newsigall.append(rncand) # save all condidate signals
-        newsigcall.append(newsigccand) # save clean signal candidates
-
-    v = []
-    for rsi in range(len(rstep)):
-        v.append(np.var(newsigall[rsi]))
-
-    rn = newsigall[find(v == min(v))[0]]
-    newsigc = newsigcall[find(v == min(v))[0]]
+for ii in range(10):
+    ri += 1
+    # run through all rstep and find best spacing for next component
+    rn, newsigc, sp, perms, permn = ed.rankdata_multi(rn, pkeys, rstep, Lvec)
+    newsig += newsigc
 
     plt.figure(ri)
     f, ax = plt.subplots(3, 1, figsize=(20, 20))
-    ax[0].plot(newsigc)
+    ax[0].plot(newsig)
     ax[0].plot(rno, 'r--')
     ax[0].plot(rnn, 'g--')
     ax[0].set_xlim(0, 300)
     ax[0].set_ylim(-1.5, 1.5)
 
-    ax[1].plot(sig.T)
+    ax[1].plot(rnn, 'g--')
+    ax[1].plot(rn, 'r--')
+    ax[1].plot(rn_noise, 'b--')
+    ax[1].set_xlim(0, 300)
 
-    ax[2].plot(rnn, 'b')
-    ax[2].plot(rn, 'r--')
-    ax[2].set_xlim(0, 300)
+    ax[2].plot(permn)
 
     # sampen.append(sampentropy(rn[1:100], 2, 0.2 * np.std(rn)))
     sampen.append(np.var(rn))
     sampendiff = (sampen[-2] - sampen[-1]) / sampen[-2]
-    print("Variance: Run %d = %f, permn=%d" % (ri, sampen[ri], permn[pi]))
-    ri += 1
+    print("Variance perc change: Run %d = %f" % (ri, sampendiff))
 
-# end for
+    # end for
 
 
 plt.show()
